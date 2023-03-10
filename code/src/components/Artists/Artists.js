fa2f9ecb3738372e37e7749c714297cd4a1d549b
malvinamaria <mmpolak@protonmail.com>
2023-03-10 15:56:18 +0100

    add icons

--- code/src/components/Artists/Artists.js
+++ code/src/components/Artists/Artists.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { SvgIcons } from '../SvgIcons/SvgIcons';
 import data from '../../data.json';
-import './Artists.css'
+import './Artists.css';
 
 export const Artists = () => {
   return (
@@ -9,19 +10,34 @@ export const Artists = () => {
         return (
           <div className="single-album-container" key={album.id}>
             <div className="album-cover-div">
-              <img className="album-cover" alt="Album cover" src={album.images[0].url} />
+              <img
+                className="album-cover"
+                alt="Album cover"
+                src={album.images[0].url}
+                // eslint-disable-next-line react/jsx-closing-bracket-location
+              />
+              <SvgIcons />
             </div>
             <span className="album-name">{album.name}</span>
             <div className="artist-names-div">
               {album.artists.map((artist, index) => (
-                <span className="artist-name" key={artist.id}> <a href={`https://open.spotify.com/artist/${artist.id}`} target="blank_">{artist.name}</a>
+                <span className="artist-name" key={artist.id}>
+                  {' '}
+                  <a
+                    href={`https://open.spotify.com/artist/${artist.id}`}
+                    target="blank_"
+                    // eslint-disable-next-line react/jsx-closing-bracket-location
+                  >
+                    {artist.name}
+                  </a>
                   {index < album.artists.length - 1 && ', '}
                 </span>
               ))}
             </div>
           </div>
-        )
-      })};
+        );
+      })}
+      ;
     </div>
   );
 };
